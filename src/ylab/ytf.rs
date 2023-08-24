193d260ff36ef5c99df45babd8fbdf6e86e831a4
schmettow <schmettow@web.de>
2023-08-24 18:47:41 +0200

    + it's ytf::bsu and ytf::y2b

--- src/ylab/ytf.rs
+++ src/ylab/ytf.rs
@@ -1,2 +1,76 @@
-/* YLab transport format  */
+/* YLab transport formats  */
+
+pub mod bsu {
+    /* #[derive(Serialize, Deserialize, Debug)]
+    pub enum Sensor {None, Adc1_1}*/
+
+    // data handling
+    use serde::{Serialize, Deserialize};
+    use postcard::to_vec;
+    use heapless::Vec;
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct Obs {
+        pub dev: i8,
+        pub time: i32,
+        pub read: [u32;1],
+    }
+
+    // Channel
+    use embassy_sync::channel::Channel;
+    use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
+    pub static SINK: Channel<CriticalSectionRawMutex, Obs, 2> = Channel::new();
+    
+    // USB
+    use embassy_stm32 as hal;
+    use hal::usart::Uart;
+    use hal::peripherals;
+    
+    #[embassy_executor::task]
+    pub async fn task(mut usart: Uart<'static, peripherals::USART3, peripherals::DMA1_CH3>) {
+        loop {
+            let obs = SINK.recv().await;
+            let line = 
+            usart.write(&ser).await.unwrap();
+            usart.write("\n".as_bytes()).await.unwrap();
+            }
+        }
+    }
+
+pub mod y2b {
+    /* #[derive(Serialize, Deserialize, Debug)]
+    pub enum Sensor {None, Adc1_1}*/
+
+    // data handling
+    use serde::{Serialize, Deserialize};
+    use postcard::to_vec;
+    use heapless::Vec;
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct Obs {
+        pub dev: i8,
+        pub time: i32,
+        pub read: [u32;1],
+    }
+
+    // Channel
+    use embassy_sync::channel::Channel;
+    use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
+    pub static SINK: Channel<CriticalSectionRawMutex, Obs, 2> = Channel::new();
+    
+    // USB
+    use embassy_stm32 as hal;
+    use hal::usart::Uart;
+    use hal::peripherals;
+    
+    #[embassy_executor::task]
+    pub async fn task(mut usart: Uart<'static, peripherals::USART3, peripherals::DMA1_CH3>) {
+        loop {
+            let obs = SINK.recv().await;
+            let ser: Vec<u8, 9> =   to_vec(&obs).unwrap();
+            usart.write(&ser).await.unwrap();
+            usart.write("\n".as_bytes()).await.unwrap();
+            }
+        }
+    }
 
