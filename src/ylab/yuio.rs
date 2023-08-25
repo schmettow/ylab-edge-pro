4a22baf234c7c9bb43614db754eddb4a41988fc4
schmettow <schmettow@web.de>
2023-08-25 15:02:03 +0200

    + removed display driver

--- src/ylab/yuio.rs
+++ src/ylab/yuio.rs
@@ -2,7 +2,9 @@
 pub mod led {
     // LED control
     use embassy_time::{Duration, Timer};
-    use embassy_rp::gpio::{AnyPin, Output, Level};
+    use embassy_time::Instant;
+    use super::hal as hal;
+    use hal::gpio::{AnyPin, Output, Level};
     use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
     use embassy_sync::signal::Signal;
     pub enum State {Vibrate, Blink, Steady, Interrupt, Off}
@@ -43,55 +45,4 @@ pub mod led {
                 }   
             };
         }
-}
-
-pub mod disp {
-    // I2C
-    use embassy_rp::i2c::{self};
-    use embassy_rp::peripherals::I2C0;
-    pub use heapless::String;
-    // use itoa;
-    /* use embedded_graphics::{ // <--- reactivate graphic output
-        pixelcolor::BinaryColor,
-        prelude::*,
-        image::{Image, ImageRaw},
-        text::{Baseline, Text},
-        mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
-    };*/
-    use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
-    // inter-thread communication
-    use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
-    use embassy_sync::signal::Signal;
-
-    pub type OneLine = String<20>;
-    pub type FourLines = [OneLine; 4];
-    
-    pub static TEXT: Signal<CriticalSectionRawMutex, FourLines> 
-                = Signal::new();
-
-    // Text display
-    use core::fmt::Write;
-
-    #[embassy_executor::task]
-    pub async fn task(i2c: i2c::I2c<'static, I2C0, i2c::Async>) {
-        let interface 
-            = I2CDisplayInterface::new(i2c);
-        let mut display =
-            Ssd1306::new(interface, 
-                    DisplaySize128x64, 
-                    DisplayRotation::Rotate0)
-            .into_terminal_mode();
-        display.init().unwrap();
-        let _ = display.write_str("YLab");
-                
-        loop {
-            let mesg: FourLines = TEXT.wait().await;
-            let _ = display.clear();
-            //let mut str_conv = itoa::Buffer::new(); // conversion to string
-            for row in mesg {
-                let _ = display.write_str(row.as_str());
-            }
-            
-        }
-    }
 }
