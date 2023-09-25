554d24634d5e266880e1ccfc0938ad3648d91cae
schmettow <schmettow@web.de>
2023-09-25 21:21:49 +0200

    + moved sample sending to Ytf + from 4 to 8 readings

--- src/ylab/mod.rs
+++ src/ylab/mod.rs
@@ -11,27 +11,17 @@ pub use embassy_sync::channel::Channel;
 pub use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex as Mutex;
 pub use core::sync::atomic::Ordering as Ordering;
 /// + common classes for samples from BOS:
-use heapless::String;
-use core::fmt::Write;
+use heapless::{String,Vec};
+//use postcard;
 use serde::{Serialize, Deserialize};
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Sample {
     pub dev: i8,
     pub time: i32,
-    pub read: [u16;4],
+    pub read: [u16;8],
 }
-/// + methods to convert to CSV
-impl Sample {
-    pub fn to_csv(&self) -> String<32>{
-        let mut line: String<32> = String::new();
-        core::write!(&mut line,
-                        "{},{},{},{},{},{}\n",
-                        self.time, self.dev, 
-                        self.read[0], self.read[1], 
-                        self.read[2], self.read[3],).unwrap();
-        return line
-    }
-}
+
+        
 /// ## Sub modules
 /// 
 /// + sensor banks
