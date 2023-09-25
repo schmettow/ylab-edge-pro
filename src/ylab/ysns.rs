42a28838234bea6a9ba9e9ddbb2f9e9cc99e0240
schmettow <schmettow@web.de>
2023-09-25 21:23:07 +0200

    + a bank carries 8 sensors + renamed tasks to adc_banks

--- src/ylab/ysns.rs
+++ src/ylab/ysns.rs
@@ -3,14 +3,15 @@
 
 pub mod adc {
     pub use super::super::{hal, Sample, Channel, Mutex, ytf::bsu as ybsu, Ordering};
+    //use hal::adc::Instance;
     use embassy_time::{Duration, Ticker, Instant};
-    use hal::peripherals::{ADC1, PA3, PC0, PC3, PC1};
-    use hal::peripherals::{ADC3, PF3, PF5, PF10, PF8};
+    use hal::peripherals::{ADC1, PA3, PC0, PC1, PC2, PC3, PA5, PA6, PA7 };
+    use hal::peripherals::{ADC3, PF3, PF4, PF5, PF6, PF7, PF8, PF9, PF10};
     //use hal::adc::{config::AdcConfig, config::SampleTime, Adc};
     use hal::adc::{Adc, SampleTime};
 
     /* data */
-    pub type Reading = [u16; 4];
+    pub type Reading = [u16; 8];
     
     // pub type Sample = (Instant, Reading);
 
@@ -23,12 +24,12 @@ pub mod adc {
     //type AdcPin: embedded_hal::adc::Channel<hal::adc::Adc<'static>> + hal::gpio::Pin;
 
     #[embassy_executor::task]
-    pub async fn task_0(mut adc: Adc<'static, ADC1>,
-                    mut pins: (PA3, PC0, PC3, PC1),
+    pub async fn adcbank_1(mut adc: Adc<'static, ADC1>,
+                    mut pins: (PA3, PC0, PC1, PC2, PC3, PA5, PA6, PA7),
                     hz: u64) {
         //let state: Atomic<super::State> = Atomic::new(State::Offline);
         let mut ticker = Ticker::every(Duration::from_hz(hz));
-        let mut vrefint = adc.enable_vrefint();
+        let mut _vrefint = adc.enable_vrefint();
         let mut reading: Reading;
         let mut sample: Sample;
         adc.set_sample_time(SampleTime::Cycles3);
@@ -39,7 +40,11 @@ pub mod adc {
                     reading =  [adc.read(&mut pins.0), 
                                 adc.read(&mut pins.1),
                                 adc.read(&mut pins.2),
-                                adc.read(&mut pins.3),];
+                                adc.read(&mut pins.3),
+                                adc.read(&mut pins.4), 
+                                adc.read(&mut pins.5),
+                                adc.read(&mut pins.6),
+                                adc.read(&mut pins.7),];
                     sample = Sample{dev: 0, 
                                     time: Instant::now().as_millis() as i32, 
                                     read: reading};
@@ -49,8 +54,8 @@ pub mod adc {
         }
 
     #[embassy_executor::task]
-    pub async fn task_1(mut adc: Adc<'static, ADC3>,
-                    mut pins: (PF3, PF5, PF10, PF8),
+    pub async fn adcbank_3(mut adc: Adc<'static, ADC3>,
+                    mut pins: (PF3, PF4, PF5, PF6, PF7, PF8, PF9, PF10),
                     hz: u64) {
         //let state: Atomic<super::State> = Atomic::new(State::Offline);
         const DEV: i8 = 1;
@@ -65,7 +70,11 @@ pub mod adc {
                     reading =  [adc.read(&mut pins.0),
                                 adc.read(&mut pins.1),
                                 adc.read(&mut pins.2),
-                                adc.read(&mut pins.3),];
+                                adc.read(&mut pins.3),
+                                adc.read(&mut pins.4),
+                                adc.read(&mut pins.5),
+                                adc.read(&mut pins.6),
+                                adc.read(&mut pins.7),];
                     //reading = [2,9,1,2];
                     sample = Sample{dev: DEV, 
                                     time: Instant::now().as_millis() as i32, 
@@ -74,5 +83,7 @@ pub mod adc {
                 }
             }                
         }
+
+
     }
 
