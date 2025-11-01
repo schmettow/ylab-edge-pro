d9cf1ae7438dfd34bad3ef90b96ca008d9d847db
schmettow <schmettow@web.de>
2025-11-01 14:39:41 +0100

    + ylab_breath uses yll::sen_five

--- src/bin/ylab_air.rs
+++ src/bin/ylab_air.rs
@@ -1,60 +1,23 @@
 #![no_std]
 #![no_main]
 
-/// CONFIGURATION
-///
-/// Adc
-static DEV: [bool; 3] = [true, true, false];
-static HZ: [u64; 3] = [300, 50, 30];
-const BAUD: u32 = 2_000_000;
 
-use ylab::ysns::adc as yadc;
-use ylab::ysns::yco2;
-/// + data transport/storage
-use ylab::ytfk::bsu as ybsu;
-/// # YLab Edge
-///
-/// __YLab Edge Pro__ is a sensor recording firmware for STM32 Nucleo boards.
-
-//use defmt::*;
-
-// use heapless::String;
-/// +  multi-threading with async
-/// + timing using Embassy time
-/// + peripherals
-/// use embassy_stm32 as hal;
-/// + thread-safe data transfer and control
-///
-/// + built-in ADC sensors
 use ylab::*;
+use ylab::mcu;
+use ylab::ysns::adc as yadc;
+use ylab::ytfk::bsu as ybsu;
+use ylab_lib::ysns::moi;
 
-/// ## UI task
-///
-/// The ui task only signals the state
-///
-//use ylab::yuio::disp as ydsp;
-///
 
-#[derive(
-    Debug, // used as fmt
-    Clone,
-    Copy, // because next_state
-    PartialEq,
-    Eq,
-)] // testing equality
-enum AppState {
-    Send,
-}
+#[derive(Debug,  // used as fmt
+    Clone, Copy, // because next_state
+    PartialEq, Eq, )] // testing equality
+enum AppState {Send}
 
-/// ## Init
-///
-/// + Initializing peripherals
-/// + spawning tasks
-/// + assigning periphs to tasks
 use mcu::adc;
-use mcu::dma::NoDma;
-use mcu::i2c;
+use mcu::exti::ExtiInput;
 use mcu::usart::{Config, Uart};
+use mcu::i2c;
 use mcu::{bind_interrupts, peripherals, usart};
 use {defmt_rtt as _, panic_probe as _};
 
@@ -64,85 +27,79 @@ bind_interrupts!(struct Irqs {
     I2C1_EV => i2c::EventInterruptHandler<peripherals::I2C1>;
     I2C1_ER => i2c::ErrorInterruptHandler<peripherals::I2C1>;
 });
+
 use embassy_executor::Spawner;
-use embassy_time::Delay;
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = mcu::init(Default::default());
     let mut config = Config::default();
-    config.baudrate = BAUD;
+    config.baudrate = 2_000_000;
     let usart = p.USART2;
     let tx = p.PA3;
     let rx = p.PA2;
-    let usart_dma = p.DMA1_CH6;
-    let usart = Uart::new(usart, tx, rx, Irqs, usart_dma, NoDma, config);
+    //let usart_dma = p.DMA1_CH6;
+    let usart = Uart::new(usart, tx, rx, Irqs, p.DMA1_CH6, p.DMA1_CH5, config);
     match usart {
         Ok(usart) => spawner.spawn(ybsu::task(usart)).unwrap(),
-        Err(_) => {
-            println!("USART connection failed")
-        }
+        Err(_)  => {log::debug!("USART connection failed")},
     }
-
-    println!("I2C new");
     spawner.spawn(control_task()).unwrap();
-    let i2c1 = i2c::I2c::new(
-        p.I2C1,
-        p.PB8,
-        p.PB9,
-        Irqs,
-        NoDma,
-        NoDma,
-        mcu::time::Hertz(100_000),
-        Default::default(),
-    );
+    // MOI
+    let moi_0
+        = ExtiInput::new(p.PA10,  p.EXTI10, ylab::Pull::Down,);
+    let moi_1
+        = ExtiInput::new(p.PB3, p.EXTI3, ylab::Pull::Down);
+    let moi_3
+        = ExtiInput::new(p.PD0,  p.EXTI0, ylab::Pull::Down,);
+    let moi_4
+        = ExtiInput::new(p.PD1, p.EXTI1, ylab::Pull::Down);
+    //spawner.spawn(ysns::moi::task(moi_0, moi_1, 0)).unwrap();
+    spawner.spawn(moi_task(moi_0, moi_1, moi_3, moi_4)).unwrap();
 
-    spawner
-        .spawn(ysns::SenFive::task(i2c1, Duration::from_secs(60), 2))
-        .unwrap();
-    //spawner.spawn(ydsp::task(i2c1)).unwrap();                 // optional display
-    //spawner.spawn(ylab::ysns::yco2::task(i2c1, 1)).unwrap();  // CO2 sensor
-    println!("I2C task ended");
+    //ADC
+    //let mut delay = Delay;
+    let adc1 = adc::Adc::new(p.ADC1);
+    spawner.spawn(yadc::adcbank_1(adc1,
+                                (p.PA0, p.PA1, p.PA4, p.PB0, p.PC1, p.PC0, p.PC3, p.PC2),
+                                197, 1)).unwrap();
+
+
+    let i2c1 = I2c::new(p.I2C1, p.PB8, p.PB9, Irqs, p.DMA1_CH7, p.DMA1_CH0, Default::default());
+    static I2C_BUS_1: StaticCell<SharedI2cBus> = StaticCell::new();
+    let i2c_bus_1 = I2C_BUS_1.init(Mutex::new(i2c1));
+    let i2c11 = SharedI2cDevice::new(i2c_bus_1);
+
+    spawner.spawn(sen5_task(i2c11)).unwrap();
+
+
 }
 
-/// ## Control task
-///
-/// This does nothiong at the moment
-/// READY is a shared Mutex, true when the sensor was correctly initialized.
-/// with SAMPLE the task is commanded to send data to the ybsu::SINK
+
+/*use mcu::gpio::Input;
+use mcu::gpio::Pull;
+use mcu::peripherals::{PD0, PD1, PD2, PD3};*/
+
+#[embassy_executor::task]
+async fn sen5_task(i2c: SharedI2cDevice) {
+    ylab_lib::ysns::sen_five::task(i2c,  Duration::from_secs(5), 2, ytfk::bsu::SINK.sender()).await;
+}
+
+
+#[embassy_executor::task]
+async fn moi_task(
+    pin_0: ExtiInput<'static>,
+    pin_1: ExtiInput<'static>,
+    pin_2: ExtiInput<'static>,
+    pin_3: ExtiInput<'static>)
+    {
+	moi::inner_task(pin_0, pin_1, pin_2, pin_3, 0, ylab::ytfk::bsu::SINK.sender()).await;
+}
+
 
 #[embassy_executor::task]
 async fn control_task() {
     let _state = AppState::Send;
-
-    loop {
-        Timer::after_millis(5).await;
-        if yadc::READY.load(ORD) {
-            yadc::SAMPLE.store(true, ORD);
-            println!("ADC sampling started");
-            break;
-        }
-    }
-
-    loop {
-        Timer::after_millis(5).await;
-        if yco2::READY.load(ORD) {
-            yco2::SAMPLE.store(true, ORD);
-            println!("CO2 sampling started");
-            break;
-        }
-    }
-
-    yco2::SAMPLE.store(true, ORD);
+    yadc::SAMPLE.store(true, ORD);
+    //moi::SAMPLE.store(true, ORD);
 }
-
-/*pub use core::sync::atomic::Ordering;
-//use ydsp::{FourLines, OneLine};
-#[embassy_executor::task]
-async fn control_task() {
-    let _state = AppState::Send;
-    yadc::SAMPLE.store(true, Ordering::Relaxed);
-    let title: OneLine = "YLab".try_into().unwrap();
-    let disp_text: FourLines = [ Some(title) ,None, None, None];
-    ydsp::TEXT.signal(disp_text);
-}*/
