b818147b60310aa5823c0046a363df47a46c6954
schmettow <schmettow@web.de>
2025-11-01 16:34:46 +0100

    + 2000000 baud

--- src/bin/ylab_dg.rs
+++ src/bin/ylab_dg.rs
@@ -11,7 +11,7 @@ use ylab_lib as yll;
 use yll::ysns::moi;
 
 #[derive(Debug,  // used as fmt
-    Clone, Copy, // because next_state 
+    Clone, Copy, // because next_state
     PartialEq, Eq, )] // testing equality
 enum AppState {Send}
 
@@ -34,7 +34,7 @@ use embassy_executor::Spawner;
 async fn main(spawner: Spawner) {
     let p = mcu::init(Default::default());
     let mut config = Config::default();
-    config.baudrate = 100_000;
+    config.baudrate = 2000;
     let usart = p.USART2;
     let tx = p.PA3;
     let rx = p.PA2;
@@ -46,22 +46,22 @@ async fn main(spawner: Spawner) {
     }
     spawner.spawn(control_task()).unwrap();
     // MOI
-    let moi_0 
-        = ExtiInput::new(p.PA10,  p.EXTI10, moi::Pull::Down,);
-    let moi_1 
-        = ExtiInput::new(p.PB3, p.EXTI3, moi::Pull::Down);
-    let moi_3 
-        = ExtiInput::new(p.PD0,  p.EXTI0, moi::Pull::Down,);
-    let moi_4 
-        = ExtiInput::new(p.PD1, p.EXTI1, moi::Pull::Down);
+    let moi_0
+        = ExtiInput::new(p.PA10,  p.EXTI10, ylab::Pull::Down,);
+    let moi_1
+        = ExtiInput::new(p.PB3, p.EXTI3, ylab::Pull::Down);
+    let moi_3
+        = ExtiInput::new(p.PD0,  p.EXTI0, ylab::Pull::Down,);
+    let moi_4
+        = ExtiInput::new(p.PD1, p.EXTI1, ylab::Pull::Down);
     //spawner.spawn(ysns::moi::task(moi_0, moi_1, 0)).unwrap();
     spawner.spawn(moi_task(moi_0, moi_1, moi_3, moi_4)).unwrap();
-    
+
     //ADC
     //let mut delay = Delay;
     let adc1 = adc::Adc::new(p.ADC1);
-    spawner.spawn(yadc::adcbank_1(adc1, 
-                                (p.PA0, p.PA1, p.PA4, p.PB0, p.PC1, p.PC0, p.PC3, p.PC2), 
+    spawner.spawn(yadc::adcbank_1(adc1,
+                                (p.PA0, p.PA1, p.PA4, p.PB0, p.PC1, p.PC0, p.PC3, p.PC2),
                                 197, 1)).unwrap();
 }
 
@@ -72,7 +72,7 @@ use mcu::peripherals::{PD0, PD1, PD2, PD3};*/
 
 #[embassy_executor::task]
 async fn moi_task(
-    pin_0: ExtiInput<'static>, 
+    pin_0: ExtiInput<'static>,
     pin_1: ExtiInput<'static>,
     pin_2: ExtiInput<'static>,
     pin_3: ExtiInput<'static>)
@@ -82,10 +82,8 @@ async fn moi_task(
 
 
 #[embassy_executor::task]
-async fn control_task() { 
+async fn control_task() {
     let _state = AppState::Send;
     yadc::SAMPLE.store(true, ORD);
-    moi::SAMPLE.store(true, ORD);
+    //moi::SAMPLE.store(true, ORD);
 }
-        
-
